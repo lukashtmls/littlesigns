bdf7da3c7fcf98c9f97440b7c04605315f915747
nautahtmls <lukashtmls@gmail.com>
2026-08-02 18:55:44 -0300

    0.1.9
    Configurei as imagens dos exemplos com flex-direction: row e agr ta tudo top, tambem coloquei o max-height do mesmo tamanho que o da div dos exemplos, ent ta tudo top pra colocar quantas imagens precisar nos exemplos, só falta terminar os exemplos dos cumprimentos

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// ABRIR E FECHAR CONTEÚDOS
+
+// NUMEROS
+
 document
   .querySelector("div.BotaoNumeros")
   .addEventListener("click", function () {
     document.querySelector("div.con_num").classList.toggle("oculto");
     document.querySelector("div.ConteudoNumeros").classList.toggle("hover");
+  });
+
+// CUMPRIMENTOS
+
+document
+  .querySelector("div.BotaoCumprimentos")
+  .addEventListener("click", function () {
+    document.querySelector("div.con_cum").classList.toggle("oculto");
+    document.querySelector("div.ConteudoCumprientos").classList.toggle("hover");
   });
