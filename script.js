382abce0a96bc85ade039ce24fb9dbd6ae6e9181
nautahtmls <lukashtmls@gmail.com>
2026-08-03 15:51:06 -0300

    0.4.8
    terminei os numeros, cumprimentos, animais e cores, falta o alfabeto

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ document
     document.querySelector("div.ConteudoNumeros").classList.toggle("hover");
   });
 
+// ALFABETO
+
+document
+  .querySelector("div.BotaoAlfabeto")
+  .addEventListener("click", function () {
+    document.querySelector("div.con_alf").classList.toggle("oculto");
+    document.querySelector("div.ConteudoAlfabeto").classList.toggle("hover");
+  });
+
 // CUMPRIMENTOS
 
 document
@@ -17,3 +26,19 @@ document
     document.querySelector("div.con_cum").classList.toggle("oculto");
     document.querySelector("div.ConteudoCumprientos").classList.toggle("hover");
   });
+
+// ANIMAIS
+
+document
+  .querySelector("div.BotaoAnimais")
+  .addEventListener("click", function () {
+    document.querySelector("div.con_ani").classList.toggle("oculto");
+    document.querySelector("div.ConteudoAnimais").classList.toggle("hover");
+  });
+
+// CORES
+
+document.querySelector("div.BotaoCores").addEventListener("click", function () {
+  document.querySelector("div.con_cor").classList.toggle("oculto");
+  document.querySelector("div.ConteudoCores").classList.toggle("hover");
+});
